9222cce3db63deb02e8cbe5856bfcf43dcf886bb
mxro <mxro@nowhere.com>
2014-08-12 08:37:46 +1000

    Reworking Nextweb persistence API and developing async-map abstraction Autocommit Tue Aug 12 08:37:46 EST 2014

--- src/main/java/io/nextweb/persistence/connections/PersistedMap.java
+++ src/main/java/io/nextweb/persistence/connections/PersistedMap.java
@@ -5,11 +5,24 @@ import de.mxro.async.callbacks.SimpleCallback;
 public interface PersistedMap {
 
 	public void init(SimpleCallback callback);
-	
+
+	/**
+	 * <p>
+	 * Releases resources held by this map.
+	 * <p>
+	 * Assures pending changes are written to persistence medium.
+	 * 
+	 * @param callback
+	 */
 	public void close(SimpleCallback callback);
 
+	/**
+	 * Assures pending changes are written to persistence medium.
+	 * 
+	 * @param callback
+	 */
 	public void commit(SimpleCallback callback);
 
 	public void clearCache();
-	
+
 }
