bb1b688d049b03376bf4650f6c21e4445a6f996b
mxro <mxro@nowhere.com>
2014-08-12 08:38:28 +1000

    m

--- src/main/java/io/nextweb/persistence/connections/MapConnection.java
+++ src/main/java/io/nextweb/persistence/connections/MapConnection.java
@@ -1,6 +1,9 @@
 package io.nextweb.persistence.connections;
 
 import de.mxro.async.callbacks.SimpleCallback;
+import de.mxro.async.map.AsyncMap;
+import de.mxro.async.map.PersistedMap;
+import de.mxro.async.map.SyncMap;
 
 public interface MapConnection<T> extends AsyncMap<T>, SyncMap<T>, PersistedMap {
 
