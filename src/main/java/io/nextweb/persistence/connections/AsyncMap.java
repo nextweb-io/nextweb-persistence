1988ed445908b5ca64a1e77e9db354fef49abeb2
mxro <mxro@nowhere.com>
2014-08-12 08:32:46 +1000

    Reworking Nextweb persistence API and developing async-map abstraction Autocommit Tue Aug 12 08:32:46 EST 2014

---
+++ src/main/java/io/nextweb/persistence/connections/AsyncMap.java
@@ -0,0 +1,14 @@
+package io.nextweb.persistence.connections;
+
+import de.mxro.async.callbacks.SimpleCallback;
+import de.mxro.async.callbacks.ValueCallback;
+
+public interface AsyncMap<T> {
+
+public void put(String key, T value, SimpleCallback callback);
+	
+	public void get(String key, ValueCallback<T> callback);
+
+	public void remove(String key, SimpleCallback callback);
+	
+}
--- src/main/java/io/nextweb/persistence/connections/MapConnection.java
+++ src/main/java/io/nextweb/persistence/connections/MapConnection.java
@@ -1,22 +1,15 @@
 package io.nextweb.persistence.connections;
 
 import de.mxro.async.callbacks.SimpleCallback;
-import de.mxro.async.callbacks.ValueCallback;
 
-public interface MapConnection<T> {
+public interface MapConnection<T> extends AsyncMap<T> {
 
-	public void put(String key, T value, SimpleCallback callback);
-	
-	public void get(String key, ValueCallback<T> callback);
-	
-	public T getSync(String key);
-	
-	public void remove(String key, SimpleCallback callback);
-	
 	public void close(SimpleCallback callback);
-	
+
 	public void commit(SimpleCallback callback);
-	
+
 	public void clearCache();
-	
+
+	public T getSync(String key);
+
 }
